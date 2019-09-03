05a159fb10e9d37e81b590376154385be051bbbf
Michael Erickson <ericks23@purdue.edu>
2019-09-03 12:30:33 -0400

    Moved code out of main.

--- Senior Design/src/ADCcode.c
+++ Senior Design/src/ADCcode.c
@@ -2,6 +2,27 @@
 #include "stm32f0xx.h"
 #include "stm32f0_discovery.h"
 
+void adc_init() {
+	//Based off of code from ece 362 lecture notes
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+	GPIOA->MODER |= GPIO_MODER_MODER4 | GPIO_MODER_MODER5;
+	RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
+	RCC->CR2 |= RCC_CR2_HSI14RDY;
+	while (! (RCC->CR2 & RCC_CR2_HSI14RDY));
+	ADC1->CR |= ADC_CR_ADEN;
+	while(! (ADC1->ISR & ADC_ISR_ADRDY));
+	while((ADC1->CR & ADC_CR_ADSTART));
+}
+
+void adc_loop() {
+	ADC1->CHSELR = 0;
+	ADC1->CHSELR |= ADC_CHSELR_CHSEL4;
+	while(!(ADC1->ISR & ADC_ISR_ADRDY));
+	ADC1-> CR |= ADC_CR_ADSTART;
+	while(!(ADC1->ISR & ADC_ISR_EOC));
+	//sprintf(line, "PA4: %2.2f", ADC1->DR * 3 / 4095.0);
+}
+
 void adc_calibrate() {
 	//ADC calibration; this code block is from STM32 reference manual
 	/* (1) Ensure that ADEN = 0 */
--- Senior Design/src/main.c
+++ Senior Design/src/main.c
@@ -17,33 +17,14 @@
 
 #include "stm32f0xx.h"
 #include "stm32f0_discovery.h"
-#include "snippets.c"
+
+#include "ADCcode.c"
 			
 
 int main(void)
 {
-	char line[21];
-
-	//Based off of code from ece 362 lecture notes
-	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
-	GPIOA->MODER |= GPIO_MODER_MODER4 | GPIO_MODER_MODER5;
-	RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
-	RCC->CR2 |= RCC_CR2_HSI14RDY;
-	while (! (RCC->CR2 & RCC_CR2_HSI14RDY));
-	ADC1->CR |= ADC_CR_ADEN;
-	while(! (ADC1->ISR & ADC_ISR_ADRDY));
-	while((ADC1->CR & ADC_CR_ADSTART));
+	adc_init();
 	while(1) {
-		ADC1->CHSELR = 0;
-		ADC1->CHSELR |= ADC_CHSELR_CHSEL4;
-		while(!(ADC1->ISR & ADC_ISR_ADRDY));
-		ADC1-> CR |= ADC_CR_ADSTART;
-		while(!(ADC1->ISR & ADC_ISR_EOC));
-		//sprintf(line, "PA4: %2.2f", ADC1->DR * 3 / 4095.0);
+		adc_loop();
 	}
-}
-
-
-
-
 }
